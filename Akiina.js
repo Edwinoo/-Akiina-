4901a05ff095ad6f3edd5913670c34158b8dc77e
Edw1in <38879990+Edwinoo@users.noreply.github.com>
2019-03-02 21:38:30 +0100

    Update Akiina.js

--- Akiina.js
+++ Akiina.js
@@ -6,8 +6,8 @@ var prefix = ("<")
 client.login(process.env.TOKEN);
 
 const activities_list = (
-    `<Aide | ${client.guilds.size} Servers | ${client.users.size} Users `, 
-    `<Aide | ${client.guilds.size} Servers | ${client.users.size} Users `,
+    "<Aide ", 
+    "<Aide ",
     "Discord ► https://discord.gg/dehgnGp", 
     "By Edwin [V4.7]"
     );
