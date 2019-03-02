e0ec3ff1473ad1e51838320afa96520c23d953df
Edw1in <38879990+Edwinoo@users.noreply.github.com>
2019-03-02 21:40:05 +0100

    Update Akiina.js

--- Akiina.js
+++ Akiina.js
@@ -5,12 +5,12 @@ var prefix = ("<")
 
 client.login(process.env.TOKEN);
 
-const activities_list = (
+const activities_list = [
     "<Aide ", 
     "<Aide ",
     "Discord ► https://discord.gg/dehgnGp", 
     "By Edwin [V4.7]"
-    );
+    ];
 
 client.on('ready', () => {
     setInterval(() => {
